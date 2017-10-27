08f1f0d233db1a9f188572c76ac6fa2eb28aae15
tenwiz <nethe0002@gmail.com>
2017-10-28 00:28:38 +0800

    reducers usersDucks

--- reducers.js
+++ reducers.js
@@ -342,3 +342,62 @@ export default function likeCount(state = initialState, action) {
       return state
   }
 }
+
+// UsersDucks
+
+const initialUsersDuckState = {
+  lastUpdated: 0,
+  duckIds: []
+}
+
+function usersDuck(state = initialUsersDuckState, action) {
+  switch (action.type) {
+    case ADD_SINGLE_USERS_DUCK:
+      return {
+        ...state,
+        duckIds: state.duckIds.concat([action.duckId])
+      }
+    default:
+      return state
+  }
+}
+
+const initialState = {
+  isFetching: true,
+  error: ''
+}
+
+export default function usersDucks(state = initialState, action) {
+  switch (action.type) {
+    case FETCHING_USERS_DUCKS:
+      return {
+        ...state,
+        isFetching: true,
+      }
+    case FETCHING_USERS_DUCKS_ERROR:
+      return {
+        ...state,
+        isFetching: false,
+        error: action.error
+      }
+    case FETCHING_USERS_DUCKS_SUCCESS:
+      return {
+        ...state,
+        isFetching:false,
+        error: '',
+        [action.uid]: {
+          lastUpdated: action.lastUpdated,
+          duckIds: action.duckIds
+        }
+      }
+    case ADD_SINGLE_USERS_DUCK:
+      return typeof state[action.uid] === 'undefined'
+        ? state
+        : {
+          ...state,
+          duckIds: usersDuck(state[action.uid], action)
+        }
+    default:
+      return state
+  }
+}
