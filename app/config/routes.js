f1323b6d3da94331ecc7e4869a03cfe90ad0e2e7
tenwiz <nethe0002@gmail.com>
2017-11-01 00:25:50 +0800

    add react-router-dom

---
+++ app/config/routes.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { BrowserRouter, Route } from 'react-router-dom'
+import { MainContainer } from '../containers'
+
+const routes = (
+  <BrowserRouter>
+    <Route path='/' component={MainContainer} />
+  </BrowserRouter>
+)
+
+export default routes
--- app/index.js
+++ app/index.js
@@ -1,15 +1,8 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
-
-class Main extends React.Component{
-  render() {
-    return (
-      <p>Hello World!</p>
-    )
-  }
-}
+import routes from './config/routes'
 
 ReactDOM.render(
-  <Main />,
+  routes,
   document.getElementById('app')
 )
