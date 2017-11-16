6b924bb5e25f4db3de9ce8432d1bd04f33ee0552
tenwiz <nethe0002@gmail.com>
2017-11-17 00:08:36 +0800

    Connecting our first React Component to Redux

--- app/containers/Authenticate/AuthenticateContainer.js
+++ app/containers/Authenticate/AuthenticateContainer.js
@@ -1,22 +1,44 @@
 import React, {Component} from 'react'
+import PropTypes from 'prop-types'
 import { Authenticate } from 'components'
 import auth from 'helpers/auth'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+import * as userActionCreators from 'redux/modules/users'
 
 class AuthenticateContainer extends Component {
   handleAuth = () => {
+    const { fetchingUser, fetchingUserSuccess, authUser, fetchingUserFailure } = this.props
+
+    fetchingUser()
     auth().then(user => {
-      console.log(user)
-    })
+      fetchingUserSuccess(user.uid, user, Date.now())
+      authUser(user.uid)
+    }).catch(error => fetchingUserFailure(error))
   }
 
   render () {
+    const { isFetching, error } = this.props
+
     return (
       <Authenticate
         onAuth={this.handleAuth}
-        isFetching={false}
-        error={''} />
+        isFetching={isFetching}
+        error={error} />
     )
   }
 }
 
-export default AuthenticateContainer
+AuthenticateContainer.propTypes = {
+  fetchingUser: PropTypes.func.isRequired,
+  fetchingUserFailure: PropTypes.func.isRequired,
+  fetchingUserSuccess: PropTypes.func.isRequired,
+  authUser: PropTypes.func.isRequired,
+  isFetching: PropTypes.bool.isRequired,
+  error: PropTypes.string.isRequired,
+}
+
+export default connect(
+  state => ({isFetching: state.isFetching, error: state.error}),
+  dispatch => bindActionCreators(userActionCreators, dispatch)
+)(AuthenticateContainer)
--- app/index.js
+++ app/index.js
@@ -1,7 +1,18 @@
+import React from 'react'
 import ReactDOM from 'react-dom'
 import routes from 'config/routes'
+import users from 'redux/modules/users'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+
+const store = createStore(
+  users,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
 
 ReactDOM.render(
-  routes,
+  <Provider store={store}>
+    {routes}
+  </Provider>,
   document.getElementById('app')
 )
